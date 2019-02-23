3c3bc09322c34ef40c29cf34c4d52c7acce23c33
P Bories <superparisian@gmail.com>
2019-02-23 12:23:09 +0100

    updated image URL

--- faker.php
+++ faker.php
@@ -1,5 +1,5 @@
 <?php
-$data = file_get_contents('http://thispersondoesnotexist.com');
+$data = file_get_contents('http://thispersondoesnotexist.com/image');
 $img = imagecreatefromstring($data);
 imagejpeg($img, 'fake.jpg');
 echo '<img src="fake.jpg?' . uniqid() . '" height="400" alt="fake face">';
